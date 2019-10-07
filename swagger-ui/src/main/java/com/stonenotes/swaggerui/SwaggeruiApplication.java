bfe55550b9e82d6d9e7ac7c31fcd92a6ff40c71a
javan <javan@orientalgame99.com>
2019-10-06 23:47:55 -0400

    增加swaggerui

---
+++ swagger-ui/src/main/java/com/stonenotes/swaggerui/SwaggeruiApplication.java
@@ -0,0 +1,16 @@
+package com.stonenotes.swaggerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: stonenotes
+ * @description ${description}
+ * @date: 2019/10/6
+ */
+@SpringBootApplication
+public class SwaggeruiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggeruiApplication.class, args);
+    }
+}
---
+++ swagger-ui/src/main/java/com/stonenotes/swaggerui/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.stonenotes.swaggerui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: stonenotes
+ * @description ${description}
+ * @date: 2019/10/6
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
